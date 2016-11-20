c8da4f5589afdc081f2582ab18f1dd4ff94bcd55
Zashel <ivan.uria@gmail.com>
2016-11-20 16:38:35 +0100

    Added VDeck Methods. Oh, yeah!

--- __init__.py
+++ __init__.py
@@ -10,6 +10,7 @@ For I can't help falling in love with you"""
                                                     #And I'm a fool
 
 import enum
+import random
 import .vcardtypes
 
 #------------ VDeck ------------#
@@ -39,6 +40,12 @@ class VDeck():
             self._status = VCard.Status.Deck
             self._visible = False
             self._properties = properties
+            self._lists = (
+                    self._hands,
+                    self._table,
+                    self._deck,
+                    self._discards
+                    )
             
         #------------ VDeck.VCard.__dir__ ------------#
         def __dir__(self):
@@ -48,7 +55,8 @@ class VDeck():
         #------------ VDeck.VCard.__getattr__ ------------#
         def __getattr__(self, attr):
             """Let's get properties as attributes.
-            As you can read in pydoc first of all it searches in __getattribute__ so..."""
+            As you can read in pydoc first of all it searches in 
+            __getattribute__ so..."""
             if attr in self.properties:
                 return self.properties[attr]
             else:
@@ -92,6 +100,7 @@ class VDeck():
             "Set a property of the card"""
             self._properties[property_key] = value
             
+    #------------ VDeck.__init__ ------------#
     def __init__(self, vdeck_type, cards=dict()):
         """Initialize the VDeck
         vdeck_type: a VDeck.Type enum
@@ -103,6 +112,116 @@ class VDeck():
         else:
             cards = vcardtypes.selector(vdeck_type)
         self._cards = [VDeck.VCard(name, cards[name]) for name in cards]
+        self._deck = self._cards.copy() #Anyway it would be referenced
+        self._hands = list()
+        self._discards = list()
+        self._off_game = list()
+        
+    #------------ VDeck methods ------------#
+    def _change_status(self, card, new_status, lists=self._lists):
+        _status = VDeck.VCard.Status #This, children, is an alias
+        assert isinstance(card, VDeck.VCard)
+        assert isinstance(new_status, _status)
+        status_dict = {
+                _status.Deck: self._deck,
+                _status.Hand: self._hands,
+                _status.Discarded: self._discarded,
+                _status.OffGame: self._off_game,
+                _status.Table: self. 
+                }
+        for lst in lists:
+            if card in lst:
+                card.set_status(status)
+                status_dict[status].append(card)
+                lst.remove(card)
+                break
+                
+    def destroy(self, card):
+        """Destroy the card wherever it is"""
+        #assert isinstance(card, VDeck.VCard) Not neccesary
+        self._change_status(card, VDeck.VCard.Status.OffGame)
+        
+    def discard(self, card):
+        """Discard the card wherever it is"""
+        lists = (
+                self._hands,
+                self._table,
+                self._deck
+                )
+        self._change_status(card, VDeck.VCard.Status.Discarded, lists)
+                
+    def draw(self):
+        """Draw a card, it goes to hand"""
+        card = self._deck.pop()
+        card.set_status(VDeck.VCard.Status.Hand)
+        self._hands.append(card)
+        
+    def play(self, card):
+        """Play the card wherever it is"""
+        lists = (
+                self._hands,
+                self._discarded,
+                self._deck
+                )
+        self._change_status(card, VDeck.VCard.Status.Table, lists)
+                
+    def recover(self, card):
+        """Draw a card from cementery. Discarded pile I mean."""
+        if card in self._discarded:
+            card.set_status(VDeck.VCard.Status.Hand)
+            self._hand.append(card)
+            self._discarded.remove(card)
+        
+    def shuffle_deck(self):
+        """Suffle the deck, you fool!"""
+        random.shuffle(self._deck) #I thought it would be harder. I love random
+        
+    def shuffle_discarded(self):
+        """Shuffle the discarded pile"""
+        random.shuffle(self._discarded)
+        
+    def _shuffle_x_on_deck(self, x):
+        """Shuffle whatever on deck""":
+        lists = (
+                self._discarded,
+                self._hands,
+                self._table,
+                self._off_game
+                )
+        is_card = False
+        if isinstance(x, VDeck.VCard):
+            x = [x]
+        else:
+            assert x in lists
+        for card in x:
+            card.set_status(VDeck.VCard.Status.Deck)
+        self._deck.extend(x)
+        if is_card is True:
+            
+        else:
+            lists[lists.index(x)] = list() #Varible scopes. ;)
+        self.shuffle_deck() #DO NOT REPEAT YOURSELF!
+            
+        
+    def shuffle_discarded_on_deck(self):
+        """Shuffle the discarded pile on deck"""
+        self._shuffle_x_on_deck(self._discarded) #What did I say about repeting?
+        
+    def shuffle_hands_on_deck(self):
+        """Shuffle the hands on deck"""
+        self._shuffle_x_on_deck(self._discarded)
+        
+    def shuffle_table_on_deck(self):
+        """Shuffle played on deck"""
+        self._shuffle_x_on_deck(self._table)
+        
+    def shuffle_off_game_on_deck(self):
+        """Shuffle off-game cards on deck"""
+        self._shuffle_x_on_deck(self._off_game)
+        
+    def shuffle_card_on_deck(self, card): #In aras of readibility
+        """Shuffle single card on deck"""
+        self._shuffle_x_on_deck(self, card)
         
 class Game():
     #TODO
