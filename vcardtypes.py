cd7da18d7a522ddd7f96d325d9aab277a10a0a49
Zashel <ivan.uria@gmail.com>
2016-11-21 20:36:05 +0100

    Added Spanish and Spanish50 set

--- vcardtypes.py
+++ vcardtypes.py
@@ -17,29 +17,58 @@ class Types(enum.Enum):
 def selector(vcard_type):
     """Returns a dictionary with name and properties of chosen preset deck"""
     #French Deck
-    deck = dict()
-    if vcard_type is Types.French:
-        def french(suit, value, color):
-            return {
-                    "suit": suit,
-                    "value": value,
-                    "color": color
-                    }
-        suits = [
+    deck = dict()   
+    french_suits = [
             "Spades",
             "Hearts",
             "Clovers",
             "Tiles"
             ]
-        colors = [
+    spanish_suits = [
+            "Oros",
+            "Copas",
+            "Espadas",
+            "Bastos"
+            ]
+    colors = [
             "Black",
             "Red"
             ]
+    def spanish_deck(values):
+        deck = dict()
+         for value in values:
+            for ind_suit, suit in enumerate(spanish_suits):
+                name = str(value)
+                if value == 10:
+                    name = "Sota"
+                elif value == 11:
+                    name = "Caballo"
+                elif value == 3:
+                    name = "Rey"
+                mus = value
+                if value == 2:
+                    mus = 1
+                elif value == 3:
+                    mus = 10
+                elif value > 10:
+                    mus = 10
+                name = "{} de {}".format(name, suit)
+                deck[name] = dict(suit=suit, value=value, mus=mus)
+        return deck
+    if vcard_type is Types.French:
         values = list(range(1, 11))
         values.extend(("J", "Q", "K"))
         for value in values:
-            for ind_suit, suit in enumerate(suits):
+            for ind_suit, suit in enumerate(french_suits):
                 name = "{} of {}".format(value is not 1 and str(value) or "Ace", suit)
-                deck[name] = french(suit, value, colors[ind_suit%2])
+                deck[name] = dict(suit=suit, value=value, color=colors[ind_suit%2])
+    elif vcard_type is Types.Spanish:
+        values = list(range(1,8))
+        values.extend(range(10,13))
+        deck = spanish_deck(values)
+    elif vcard_type is Types.Spanish50:
+        values = list(range(1,13))
+        deck = spanish_deck(values)
+                
     return deck
 
